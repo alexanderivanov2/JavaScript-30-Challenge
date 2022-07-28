eca03d0716bcdf3e398abb1580419a6671b45c37
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-07-28 17:52:43 +0300

    set functionality of clock

---
+++ 02-JavaScript-And-CSS-Clock/app.js
@@ -0,0 +1,39 @@
+let parameters = {};
+
+window.addEventListener('load', (e) => startClock(e));
+
+function startClock(e) {
+    const hourHand = document.querySelector('.hour-hand');
+    const minHand = document.querySelector('.min-hand');
+    const secHand = document.querySelector('.second-hand');
+    // const time = new Date(Date.now());
+    parameters = {hourHand, minHand, secHand};
+    
+    function workClock() {
+        const time = new Date(Date.now());
+        let hour = time.getHours();
+        let mins = time.getMinutes();
+        let seconds = time.getSeconds();
+    
+        let hourDeg = convertHours(hour);
+        let minsDeg = converMinutesOrSeconds(mins);
+        let secDeg = converMinutesOrSeconds(seconds);
+        console.log(`${hour}:${mins}:${seconds}`);
+        hourHand.style.transform = `rotate(${hourDeg}deg)`;
+        minHand.style.transform = `rotate(${minsDeg}deg)`;
+        secHand.style.transform = `rotate(${secDeg}deg)`;
+        
+        setTimeout(() => workClock(), 1000);
+    }
+     
+    workClock();
+}
+
+function convertHours(hour) {
+    return Math.abs(hour - 9) * 30;
+}
+
+function converMinutesOrSeconds(n) {
+    return Math.abs(n * 6) + 90;
+}
+
