f06c3e5cba0700d0d57f5dca3891c81ed53590bb
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-07-28 12:38:01 +0300

    refactor

--- 01-JavaScript-Drum-Kit/app.js
+++ 01-JavaScript-Drum-Kit/app.js
@@ -1,25 +1,32 @@
-
 const drumKeys = {
-    KeyA: {'data-key': '65', soundTime: 500},
-    KeyS: {'data-key':'83', soundTime: 500},
-    KeyD: {'data-key':'68', soundTime: 500},
-    KeyF: {'data-key':'70', soundTime: 1000},
-    KeyG: {'data-key':'71', soundTime: 800},
-    KeyH: {'data-key':'72', soundTime: 800},
-    KeyJ: {'data-key':'74', soundTime: 500},
-    KeyK: {'data-key':'75', soundTime: 600},
-    KeyL: {'data-key':'76', soundTime: 500},
+    KeyA: {'data-key': '65'},
+    KeyS: {'data-key':'83'},
+    KeyD: {'data-key':'68'},
+    KeyF: {'data-key':'70'},
+    KeyG: {'data-key':'71'},
+    KeyH: {'data-key':'72'},
+    KeyJ: {'data-key':'74'},
+    KeyK: {'data-key':'75'},
+    KeyL: {'data-key':'76'},
 }
 
-window.addEventListener('keydown', (e) => {
+window.addEventListener('load', (e) => {
     e.preventDefault();
+    const keys = document.querySelectorAll('.key');
+    
+    keys.forEach(key => key.addEventListener('transitionend', (e) => {
+        if (e.propertyName !== 'transform') return;
+        e.target.classList.remove('playing');
+    }));
+});
 
+window.addEventListener('keydown', (e) => {
+    // e.preventDefault();
     const pressedKey = e.code;
     const drumKey = drumKeys[pressedKey];
 
     if (drumKey) {
         const dataKey = drumKey['data-key'];
-        const soundTimeout = drumKey.soundTime;
 
         const divDrumKey = document.querySelector(`div[data-key="${dataKey}"]`);
         const audioDrumKey = document.querySelector(`audio[data-key="${dataKey}"]`);    
@@ -27,7 +34,5 @@ window.addEventListener('keydown', (e) => {
         divDrumKey.classList.add('playing');
         audioDrumKey.currentTime = 0;
         audioDrumKey.play();
-
-        setTimeout(() => divDrumKey.classList.remove('playing'), soundTimeout);
     }
 });
