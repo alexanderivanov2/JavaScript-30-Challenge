b2aeb97200917579eb60931ca406be2fd5988887
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-07-29 20:27:49 +0300

    solution

--- 05-Flex-Panel-Gallery/app.js
+++ 05-Flex-Panel-Gallery/app.js
@@ -8,22 +8,38 @@ function flexTranformation(e) {
     console.log('hi');
     const panel = e.currentTarget;
     const p1 = panel.children[0];
+    const pMiddle = panel.children[1];
     const p2 = panel.children[2];
     const panelFlexGrow = panel.style.flexGrow;
     console.log(panelFlexGrow);
     if (panelFlexGrow == '' || panelFlexGrow == '1') {
         p1.classList.remove('closed');
         p2.classList.remove('closed');
+        
+        p1.classList.remove('closePanelOne');
+        p2.classList.remove('closePanelTwo');
+        pMiddle.style.fontSize = "8rem";
+        // pMiddle.classList.add('openMiddle');
         p1.classList.add('openPanelOne');
         p2.classList.add('openPanelTwo');
+
         panel.style.flexGrow = '5';
     } else {
-        panel.style.flexGrow = '1';
+        
+        pMiddle.classList.remove('.OpenMiddle')
         p1.classList.remove('openPanelOne');
         p2.classList.remove('openPanelTwo');
-        p1.classList.add('closed');
-        p2.classList.add('closed');
+
+        pMiddle.style.fontSize = "4rem";
+        p1.classList.add('closePanelOne');
+        p2.classList.add('closePanelTwo');
+
+        panel.style.flexGrow = '1';
+
+        setTimeout(() => {
+            p1.classList.add('closed');
+            p2.classList.add('closed');
+        }, 1000);
 
     }
-    console.log(panel.style.flexGrow);
 }
