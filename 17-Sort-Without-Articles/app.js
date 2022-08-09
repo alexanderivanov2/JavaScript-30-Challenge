20c28b6ba127e0733bf19560cf40247f5495641b
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-08-09 14:53:26 +0300

    sort bands

--- 17-Sort-Without-Articles/app.js
+++ 17-Sort-Without-Articles/app.js
@@ -1,1 +1,24 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const ulElement = document.querySelector('#bands');
+const regExpMatch = new RegExp('\\b[A|An||The]+\\s', 'i');
+const isStartWithAnOrA = (txt) => regExpMatch.test(txt);
+
+bands.sort((a, b) => {
+    a = isStartWithAnOrA(a) ? a.split(' ').slice(1,).join(' ') : a;
+    b = isStartWithAnOrA(b) ? b.split(' ').slice(1,).join('') : b;
+
+    const result = a.localeCompare(b);
+    return result;
+});
+
+
+const fragment = document.createDocumentFragment();
+
+bands.forEach(band => {
+    const liElement = document.createElement('li');
+    liElement.textContent = band;
+    fragment.appendChild(liElement);
+});
+
+ulElement.appendChild(fragment);
