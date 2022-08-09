899cf18e61a93663470f8aa60a7e049f68d191a1
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-08-09 13:01:29 +0300

    solution

---
+++ 16-Mouse-Move-Shadow/app.js
@@ -0,0 +1,36 @@
+const h1Element = document.querySelector('h1');
+const divElement = document.querySelector('.hero');
+
+
+
+window.addEventListener('mousemove', (e) => {
+    e.preventDefault();
+    const h1OffsetTop = h1Element.offsetTop;
+    const h1offsetBottom = h1Element.offsetTop + h1Element.clientHeight;
+    const h1OffsetLeft = h1Element.offsetLeft;
+    const h1OffsetRight = h1OffsetLeft + h1Element.clientWidth;
+    // console.log(h1Element);
+    // console.log(`TOP: ${h1OffsetTop}\nBOTTOM: ${h1offsetBottom}\nLEFT: ${h1OffsetLeft}\nRIGHT: ${h1OffsetRight}`);
+    // console.log(h1Element.style.textShadow)
+    const y = e.offsetY;
+    const x = e.offsetX;
+    let shadowX = 10;
+    let shadowY = 10;
+    let color = 'black';
+    if (y < h1OffsetTop) {
+        shadowY = (h1OffsetTop - y) / 3;
+        color = 'green';
+    } else if ( y > h1offsetBottom) {
+        shadowY = -((y- h1offsetBottom) / 3);
+        color = 'blue';
+    }
+
+    if (x < h1OffsetLeft) {
+        shadowX = (h1OffsetLeft - x) / 2;
+    } else if ( x > h1OffsetRight) {
+        shadowX = -(x - h1OffsetRight) / 2;
+    }
+
+
+    h1Element.style.textShadow = `${shadowX}px ${shadowY}px 1px ${color}, ${shadowX + 10}px ${shadowY + 10}px 1px ${'red'}`;
+})
