7d9564fc11d7869b3123e162204f8350c9be1ac2
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-08-12 00:22:40 +0300

    solution

--- 20-Speech-Detection/app.js
+++ 20-Speech-Detection/app.js
@@ -1,2 +1,47 @@
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-console.log(window.SpeechRecognition);
+const divWords = document.querySelector('.words');
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let speech = new SpeechRecognition();
+speech.interimResults = true;
+speech.lang = 'en-US';
+
+
+p = document.createElement('p');
+divWords.appendChild(p);
+
+function writeResult(result) {
+ 
+
+    divWords.appendChild(p);
+
+    p.textContent = result;
+
+}
+
+
+function addNewParagraph(e) {
+    e.preventDefault();
+ 
+    if (p.textContent) {
+        p = p.cloneNode();
+        divWords.appendChild(p);
+    }
+
+}
+
+speech.addEventListener('result', (e) => {
+    let answer = Array.from(e.results)
+        .map(res => res[0])
+        .map(res => res.transcript)
+        .join(' ');
+
+    writeResult(answer);
+});
+
+
+speech.addEventListener('end', speech.start);
+speech.addEventListener('end', addNewParagraph);
+
+
+speech.start();
