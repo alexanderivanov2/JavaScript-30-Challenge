d38abf31d68018a3aa58e5634d9b8eb59c448797
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-08-12 18:41:10 +0300

    highlight move functionality
    when hover a tag move highlight over it

--- 22-Follow-Along-Link-Highlighter/app.js
+++ 22-Follow-Along-Link-Highlighter/app.js
@@ -1,10 +1,23 @@
 const aTags = document.querySelectorAll('a');
-console.log(aTags);
+const span = document.querySelector('.highlight');
+console.log(span);
 
 aTags.forEach(aTag => {
     aTag.addEventListener('mouseover', (e) => {
         e.preventDefault();
+        console.dir(e.target);
+        console.log(e);
         console.log(e.target);
-        e.target.classList.toggle('.highlight');
+        const obj = {
+            width: e.target.offsetWidth,
+            height: e.target.offsetHeight,
+            y: e.target.offsetParent.offsetTop + e.target.offsetTop ,
+            x: e.target.offsetParent.offsetLeft + e.target.offsetLeft,
+        }
+        console.log(obj);
+
+        span.style.width = obj.width  + 'px';
+        span.style.height = obj.height  + 'px';
+        span.style.transform = `translate(${obj.x}px, ${obj.y}px)`;
     });
 });
